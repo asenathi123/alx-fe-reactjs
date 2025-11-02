3de81a9567fe67cf4be04fa290058c9b94d6b081
Asenathi <sotshintshiasenathi@gmail.com>
2025-11-02 23:34:49 +0200

    style(components): add inline styles to Header, UserProfile, MainContent, and Footer

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -1,23 +1,19 @@
 import React from 'react';
 
-function Header() {
+const Header = () => {
   const headerStyle = {
     backgroundColor: 'navy',
     color: 'white',
     textAlign: 'center',
     padding: '20px',
-  };
-
-  const titleStyle = {
-    fontSize: '28px',
-    margin: 0,
+    borderRadius: '6px',
   };
 
   return (
     <header style={headerStyle}>
-      <h1 style={titleStyle}>My Favorite Cities</h1>
+      <h1>My Favorite Cities</h1>
     </header>
   );
-}
+};
 
 export default Header;
